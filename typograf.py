e5b4af38bf0b14cb632b3a2cd3fc9f3386e628f9
Anton Yakutovich <drakulavich@gmail.com>
2015-10-31 19:15:37 +0300

    add get_htmlsafe_string func, refactoring

--- typograf.py
+++ typograf.py
@@ -23,29 +23,37 @@ TEMPLATE = u"""<?xml version="1.0" encoding="UTF-8"?>
     </soap:Envelope>"""
 
 
-def typograf(text):
-    """ Returns text with russian typography """
+def get_htmlsafe(text):
+    """ Returns HTML-safe string """
 
-    # convert chars into HTML-safe sequences
     text = text.replace('&', '&amp;')
     text = text.replace('<', '&lt;')
     text = text.replace('>', '&gt;')
 
+    return text
+
+
+def typograf(text):
+    """ Returns text with russian typography """
+
+    src = get_htmlsafe(text)
     r = post('{url}typograf.asmx'.format(url=SERVICE_URL),
-             TEMPLATE.format(url=SERVICE_URL, text=text).encode('utf-8'))
+             TEMPLATE.format(url=SERVICE_URL, text=src)
+             .encode('utf-8'))
 
     if r.status_code == 200:
         root = ET.fromstring(r.content)
-        return root.findtext(
-            './/{{{url}}}ProcessTextResult'.format(url=SERVICE_URL)) \
-            .replace('<br />\n', '\n') \
-            .rstrip('\n')  # remove <br> tags and the last newline
-    else:
-        return text
+        res = root.findtext('.//{{{url}}}ProcessTextResult'
+                            .format(url=SERVICE_URL))
+        # remove br tags and last newline
+        res = res.replace('<br />\n', '\n').rstrip('\n')
+        text = res
+
+    return text
 
 
 if __name__ == '__main__':
-    print('used func:', pyperclip.copy)
+    print('used func:', pyperclip.copy.__name__)
 
     # get text from clipboard
     text = pyperclip.paste()
